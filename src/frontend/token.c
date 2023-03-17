3129a6c8629f7e2353725bee9fee9fecdea6b957
ownesis <83978267+ownesis@users.noreply.github.com>
2023-03-17 19:53:36 +0100

    init token_t before return it

--- src/frontend/token.c
+++ src/frontend/token.c
@@ -2,10 +2,8 @@
 #include "token.h"
 
 token_t init_token(token_kind_t kind, string_t* value) {
-    return (token_t) {
-        .kind = kind,
-        .value = value,
-    };
+    token_t t = {.kind = kind, .value = value};
+    return t;
 }
 
 void token_drop(token_t* self) {
