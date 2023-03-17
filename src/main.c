92649758b85ff078d2d8214b1ce1e60fbe0f4e49
hanako-eo <enuxura@gmail.com>
2023-03-17 01:11:37 +0100

    Added a frontend lexer and token, and a string utility

--- src/main.c
+++ src/main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "frontend/lexer.h"
 
-int main(int argc, char** argv)
-{
-    printf("hello world!\n");
+int main(int argc, char** argv) {
+    if (argc == 1) {
+        printf("USAGE: %s <file.cr>\n", argv[0]);
+        return 1;
+    }
+
+    lexer_t* lexer = new_lexer(argv[1]);
+    lexer_drop(lexer);
+    
     return 0;
 }
