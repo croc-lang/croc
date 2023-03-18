da658006c03e421c093ce3a41f3cb4fad57b728c
hanako-eo <enuxura@gmail.com>
2023-03-18 18:52:37 +0100

    add new string with specific size and push char

--- src/string_utils.h
+++ src/string_utils.h
@@ -7,9 +7,11 @@ typedef struct String {
 } string_t;
 
 string_t* new_string(char* str);
+string_t* sized_string(size_t len);
 char string_get(string_t* self, size_t index);
 string_t* string_slice(string_t* self, size_t start, size_t end);
 string_t* string_push(string_t* self, char* str2);
+string_t* string_push_char(string_t* self, char c);
 string_t* string_push_str(string_t* self, string_t* str2);
 void string_drop(string_t* self);
 
